9f69cc06544d51ae2cfeb55965b2045ce671ae71
Austin Wheeler <austinwheelerj@gmail.com>
2020-02-06 16:25:55 -0500

    added skip key prod

--- config/keys.js
+++ config/keys.js
@@ -4,4 +4,5 @@ module.exports = process.env.NODE_ENV === 'production' ? {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   expressSessionSecret: process.env.EXPRESS_SESSION_SECRET,
+  skip: process.env.SKIP_KEY,
 } : require('./dev');

c49f95ba933a30971408b436b2434371e1f85cbd
Austin Wheeler <austinwheelerj@gmail.com>
2020-02-07 02:13:27 -0500

    heroku test

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-if (process.env.NODE_ENV === 'production')
-    app.use(expressSslify.HTTPS({ trustProtoHeader: true }));
+// if (process.env.NODE_ENV === 'production')
+//     app.use(expressSslify.HTTPS({ trustProtoHeader: true }));
 expressWs(app, server);
 
 require('./routes/websocket')(app);
@@ -35,7 +35,8 @@ app.use(expressSession({
         collection: 'sessions'
     }),
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    secure: process.env.NODE_ENV === 'production',
 }));
 
 // Oauth Routes

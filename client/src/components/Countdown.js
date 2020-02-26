f5e0fb120242e14b18a9a446519707ef9e1b70d6
Austin Wheeler <austinwheelerj@gmail.com>
2020-02-26 12:53:53 -0500

    added simple notification system

--- client/src/components/Countdown.js
+++ client/src/components/Countdown.js
@@ -9,18 +9,28 @@ const Countdown = connect(({auth, dash}) => ({auth, dash}), actions)
   const key = props.match.params.key;
   const [timer, setTimer] = useState();
   const [displayTime, setDisplayTime] = useState();
+  const [displayExpired, setDisplayExpired] = useState(true);
+  const [notification, setNotification] = useState(false);
   const timerOut = timer && displayTime === '00:00';
   const endApproaching = timer && displayTime && !timerOut &&
     displayTime.substring(0, 2) === '00';
 
   useEffect(() => {
-    if (!timer) return;
+    if (notification && window.Notification && Notification.permission !== "denied")
+      Notification.requestPermission(() =>
+        timer.animate ? new Notification('Times Up!', {
+          body: 'The countdown you were watching has expired'
+        }) : null);
+  }, [notification, timer]);
+
+  useEffect(() => {
+    if (!timer || !displayExpired) return;
     const calc = calculateDisplay(timer, Date.now());
-    if (calc.display === displayTime) return;
-    setDisplayTime(p => calc.display);
+    setDisplayTime(calc.display);
+    setDisplayExpired(false);
     if (calc.delay && calc.display !== '00:00')
-      setTimeout(() => setDisplayTime(p => null), calc.delay);
-  }, [timer, displayTime]);
+      setTimeout(() => setDisplayExpired(true), calc.delay);
+  }, [timer, displayExpired, displayTime]);
 
   useEffect(() => {
     fetchCountdown(key)
@@ -46,23 +56,32 @@ const Countdown = connect(({auth, dash}) => ({auth, dash}), actions)
         ws.send(key);
       };
       ws.onmessage = (e) => {
-        setTimer(p => ({...p, hiddenMessage: e.data}));
+        setTimer(p => ({...p, hiddenMessage: e.data, animate: true}));
         ws.close();
       };
-      setTimeout(() => ws.close(), 1000 * 70);
+      setTimeout(() => ws.close(), 1000 * 90); // page refresh here?
     }
-  }, [timer, key, endApproaching]);
+  }, [key, endApproaching]);
 
   return !timer ? null : (
     <div className="timerContent">
-      <div className="title">
-        <p>{timer.message}</p>
+      <div className="timerUpper">
+        <div className="title">
+          <p>{timer.message}</p>
+        </div>
+        <div className="timer">
+          <p>{displayTime}</p>
+        </div>
       </div>
-      <div className="timer">
-        <p>{displayTime}</p>
-      </div>
-      <div className={"hidden " + (timer.animate ? "fadein" : "")}>
+      <div className={"hidden" + (timer.animate ? " fadein" : "")}>
         <p>{timer.hiddenMessage}</p>
+      </div>
+      <div className="settings">
+        <form>
+          <label htmlFor="notification">Notify me when this timer ends</label>
+          <input type="checkbox" id="notification" name="notification"
+                 onChange={e => setNotification(e.target.checked)}/>
+        </form>
       </div>
     </div>
   );
